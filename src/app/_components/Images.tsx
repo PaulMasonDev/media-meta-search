3b20498ec68edd073ab8d208df42aa6dca02ecef
Paul Mason <webdevpaulmason@gmail.com>
2024-06-12 15:02:31 -0700

    add missing component

---
+++ src/app/_components/Images.tsx
@@ -0,0 +1,20 @@
+import { db } from "~/server/db";
+
+export default async function Images() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image, index) => {
+        return (
+          <div key={image.id + "-" + index} className="flex w-48 flex-col">
+            <img src={image.url} alt={image.name} />
+            <div>{image.name}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
