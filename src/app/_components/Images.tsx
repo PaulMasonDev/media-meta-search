f61f89c2d7a689114dae866b2401ef043b7e362c
Paul Mason <webdevpaulmason@gmail.com>
2024-06-18 14:07:55 -0700

    add padding back on homepage

--- src/app/_components/Images.tsx
+++ src/app/_components/Images.tsx
@@ -6,28 +6,23 @@ export default async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {[...images, ...images, ...images, ...images, ...images].map(
-        (image, index) => {
-          return (
-            <div
-              key={image.id + "-" + index}
-              className="flex h-48 w-48 flex-col"
-            >
-              <Link href={`/img/${image.id}`}>
-                <Image
-                  src={image.url}
-                  style={{ objectFit: "contain" }}
-                  width={192}
-                  height={192}
-                  alt={image.name}
-                />
-              </Link>
-              <div>{image.name}</div>
-            </div>
-          );
-        },
-      )}
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {images.map((image, index) => {
+        return (
+          <div key={image.id + "-" + index} className="flex h-48 w-48 flex-col">
+            <Link href={`/img/${image.id}`}>
+              <Image
+                src={image.url}
+                style={{ objectFit: "contain" }}
+                width={192}
+                height={192}
+                alt={image.name}
+              />
+            </Link>
+            <div>{image.name}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
