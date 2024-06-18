0d2ad1ac143ee8ffb820449c3c15094d8a50787d
Paul Mason <webdevpaulmason@gmail.com>
2024-06-18 14:44:04 -0700

    shad ui init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
-        <body className="">
+        <body className="dark">
           <NextSSRPlugin
             /**
              * The `extractRouterConfig` will extract **only** the route configs
