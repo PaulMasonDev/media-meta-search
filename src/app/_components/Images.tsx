70105403b424f60f7bcec6ec1929708de83d5525
Paul Mason <webdevpaulmason@gmail.com>
2024-06-18 13:43:23 -0700

    final styling

--- src/app/_components/Images.tsx
+++ src/app/_components/Images.tsx
@@ -7,22 +7,27 @@ export default async function Images() {
 
   return (
     <div className="flex flex-wrap justify-center gap-4">
-      {images.map((image, index) => {
-        return (
-          <div key={image.id + "-" + index} className="flex h-48 w-48 flex-col">
-            <Link href={`/img/${image.id}`}>
-              <Image
-                src={image.url}
-                style={{ objectFit: "contain" }}
-                width={192}
-                height={192}
-                alt={image.name}
-              />
-            </Link>
-            <div>{image.name}</div>
-          </div>
-        );
-      })}
+      {[...images, ...images, ...images, ...images, ...images].map(
+        (image, index) => {
+          return (
+            <div
+              key={image.id + "-" + index}
+              className="flex h-48 w-48 flex-col"
+            >
+              <Link href={`/img/${image.id}`}>
+                <Image
+                  src={image.url}
+                  style={{ objectFit: "contain" }}
+                  width={192}
+                  height={192}
+                  alt={image.name}
+                />
+              </Link>
+              <div>{image.name}</div>
+            </div>
+          );
+        },
+      )}
     </div>
   );
 }
