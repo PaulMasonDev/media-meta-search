7de6557d7267c43da8a75d72ad2d59eef001409b
Paul Mason <webdevpaulmason@gmail.com>
2024-07-01 07:49:59 -0700

    refactor: Update grid gap in SearchResults component
    The code changes modify the grid gap property in the SearchResults component, increasing it from 4 to 8. This change improves the visual spacing between the search results, providing a more balanced and aesthetically pleasing layout.

--- src/app/search/components/ShowResult.tsx
+++ src/app/search/components/ShowResult.tsx
@@ -2,7 +2,7 @@ import { type TvShow } from "~/search-types";
 
 export const ShowResult = ({ show }: TvShow) => {
   return (
-    <div className="flex flex-col gap-4 overflow-hidden rounded-lg bg-white p-4 text-black shadow-lg">
+    <div className="flex flex-col overflow-hidden rounded-lg bg-white p-4 text-black shadow-lg">
       <div className="flex flex-col items-center">
         <h1>{show.name}</h1>
         <img src={show.image.medium} alt={show.name} />
