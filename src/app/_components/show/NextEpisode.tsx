9e5249ff43ed77f90ed426c42b56ce73c0896425
Paul Mason <webdevpaulmason@gmail.com>
2024-08-01 16:25:06 -0700

    space issue on next episode

--- src/app/_components/show/NextEpisode.tsx
+++ src/app/_components/show/NextEpisode.tsx
@@ -6,7 +6,7 @@ export const NextEpisode = ({ nextEpisode }: { nextEpisode: TvEpisode }) => {
   return (
     <h3 className="flex flex-wrap">
       <div className="">
-        {`S${nextEpisode.season}:E${nextEpisode.number} ${nextEpisode.name !== "TBA" ? `"${nextEpisode.name}"` : ""}`}
+        {`S${nextEpisode.season}:E${nextEpisode.number} ${nextEpisode.name !== "TBA" ? `"${nextEpisode.name} "` : ""}`}
         airing on {new Date(nextEpisode?.airdate).toLocaleDateString()}
       </div>
     </h3>
