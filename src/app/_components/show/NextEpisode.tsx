a38fb40ca846534bb1cb8fa12751ce93cd764a33
Paul Mason <63802301+PaulMasonDev@users.noreply.github.com>
2024-08-02 21:28:04 -0700

    Update NextEpisode.tsx

--- src/app/_components/show/NextEpisode.tsx
+++ src/app/_components/show/NextEpisode.tsx
@@ -3,11 +3,14 @@
 import { type TvEpisode } from "~/server/types/search-types";
 
 export const NextEpisode = ({ nextEpisode }: { nextEpisode: TvEpisode }) => {
+  const utcDate = new Date(`${nextEpisode?.airdate}T00:00:00Z`);
+  const formattedDate = utcDate.toLocaleDateString();
+  
   return (
     <h3 className="flex flex-wrap">
       <div className="">
         {`S${nextEpisode.season}:E${nextEpisode.number} ${nextEpisode.name !== "TBA" ? `"${nextEpisode.name} "` : ""}`}
-        airing on {new Date(nextEpisode?.airdate).toLocaleDateString()}
+        airing on {formattedDate}
       </div>
 <span>{nextEpisode?.airdate}</span>
     </h3>
