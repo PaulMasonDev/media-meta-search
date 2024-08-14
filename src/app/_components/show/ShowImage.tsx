21d5647a605ed4dd08ca1b6a2641ec08b0f7f615
Paul Mason <webdevpaulmason@gmail.com>
2024-08-14 14:25:42 -0700

    phase 1 h1 replacement for missing images

--- src/app/_components/show/ShowImage.tsx
+++ src/app/_components/show/ShowImage.tsx
@@ -3,24 +3,30 @@ import { type TvShow } from "~/server/types/search-types";
 
 const ShowImage = ({ show }: { show: TvShow }) => {
   return (
-    <figure>
-      <Link
-        href={show.officialSite ?? show.url}
-        referrerPolicy="no-referrer"
-        target="_blank"
-      >
-        <img src={show.image?.medium} width={400} alt={show.name} />
-      </Link>
-      <figcaption className="text-center text-xs">
-        Image © TV Maze, used under{" "}
-        <a
-          href="https://creativecommons.org/licenses/by-sa/4.0/"
-          target="_blank"
-        >
-          CC BY-SA 4.0
-        </a>
-      </figcaption>
-    </figure>
+    <>
+      {show.image?.medium ? (
+        <figure>
+          <Link
+            href={show.officialSite ?? show.url}
+            referrerPolicy="no-referrer"
+            target="_blank"
+          >
+            <img src={show.image?.medium} width={400} alt={show.name} />
+          </Link>
+          <figcaption className="text-center text-xs">
+            Image © TV Maze, used under{" "}
+            <a
+              href="https://creativecommons.org/licenses/by-sa/4.0/"
+              target="_blank"
+            >
+              CC BY-SA 4.0
+            </a>
+          </figcaption>
+        </figure>
+      ) : (
+        <h2 className="text-center">{show.name}</h2>
+      )}
+    </>
   );
 };
 
