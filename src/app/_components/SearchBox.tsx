9639f6634e7c94df596ab378bd845ff81d9c205a
Paul Mason <63802301+PaulMasonDev@users.noreply.github.com>
2025-04-25 21:58:00 -0700

    Update SearchBox.tsx

--- src/app/_components/SearchBox.tsx
+++ src/app/_components/SearchBox.tsx
@@ -20,7 +20,7 @@ const SearchBox = () => {
     <div className="search-box">
       <form onSubmit={handleSubmit}>
         <input
-          className="w-40 rounded-xl p-2 text-black focus:outline-none focus:ring-2 focus:ring-blue-400 sm:w-56"
+          className="w-full rounded-xl p-2 text-black focus:outline-none focus:ring-2 focus:ring-blue-400 sm:w-56"
           type="search"
           value={searchTerm}
           onChange={handleChange}
